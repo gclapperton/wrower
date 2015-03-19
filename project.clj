326b04dfe01ef747a7c05ddbc3829a36abae4f18
Gareth <gclapperton@live.com>
2015-03-19 15:47:39 -0400

    test: Version 0.1.10

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wrower "0.1.10-SNAPSHOT"
+(defproject wrower "0.1.10"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
