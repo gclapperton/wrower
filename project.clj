041d096ded8c89310c6094f529f951841455d75e
Gareth <gclapperton@live.com>
2015-03-19 18:59:46 -0400

    wrower: Version 0.1.12-release4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wrower "0.1.12-release3"
+(defproject wrower "0.1.12-release4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
