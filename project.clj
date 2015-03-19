6ba63743a1d62da7bad25a2f29c58462b1c5cbb2
Gareth <gclapperton@live.com>
2015-03-18 20:17:33 -0400

    Test release tag prefix

--- project.clj
+++ project.clj
@@ -5,4 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [clj-serial "2.0.2"]])
+                 [clj-serial "2.0.2"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "test"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
